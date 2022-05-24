89be0a366b688c4e2031d82c54a44d2c7e9d79e6
Pierre Lopez <pierre_albert_lopez@hotmail.fr>
2022-05-24 20:02:14 +0200

    Initialisation

---
+++ src/main/java/plopez/biblianime/BibliAnimeApplication.java
@@ -0,0 +1,13 @@
+package plopez.biblianime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliAnimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliAnimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/plopez/biblianime/BibliAnimeApplicationTests.java
@@ -0,0 +1,13 @@
+package plopez.biblianime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliAnimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
