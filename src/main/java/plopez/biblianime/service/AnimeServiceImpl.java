bcfbf9f8434f07ac5dd3ae4d4a48620feb9cf748
Pierre Lopez <pierre_albert_lopez@hotmail.fr>
2022-05-28 10:12:40 +0200

    Implementation de update

--- src/main/java/plopez/biblianime/service/AnimeServiceImpl.java
+++ src/main/java/plopez/biblianime/service/AnimeServiceImpl.java
@@ -25,7 +25,27 @@ public class AnimeServiceImpl implements AnimeService {
 
     @Override
     public Anime updateAnime(Anime anime, Long animeId) {
-        return null;
+
+        Anime animeEntity = animeRepository.findById(animeId).orElse(new Anime());
+
+        if (animeEntity.getStatut() != Statut.EN_COUR && anime.getStatut() == Statut.EN_COUR) {
+            animeEntity.setDateDebut(LocalDate.now());
+        }
+
+        if (animeEntity.getStatut() != Statut.FINIT && anime.getStatut() == Statut.FINIT) {
+            animeEntity.setDateFin(LocalDate.now());
+        }
+        animeEntity.setTitres(anime.getTitres());
+        animeEntity.setStatut(anime.getStatut());
+        animeEntity.setNote(anime.getNote());
+        animeEntity.setType(anime.getType());
+        animeEntity.setNbEpisodeVue(anime.getNbEpisodeVue());
+        animeEntity.setNbEpisodeTotal(anime.getNbEpisodeTotal());
+        animeEntity.setNautiljon(anime.getNautiljon());
+        animeEntity.setWikipedia(anime.getWikipedia());
+        animeEntity.setCommentaire(anime.getCommentaire());
+
+        return animeRepository.save(animeEntity);
     }
 
     @Override
