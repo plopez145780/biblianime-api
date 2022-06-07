e4327f24d7b955ac97023106ea0c3afb2204ee29
Pierre Lopez <pierre_albert_lopez@hotmail.fr>
2022-06-07 09:29:00 +0200

    Mise en forme de AnimeService

--- src/main/java/plopez/biblianime/anime/service/AnimeService.java
+++ src/main/java/plopez/biblianime/anime/service/AnimeService.java
@@ -6,16 +6,10 @@ import plopez.biblianime.anime.entity.Statut;
 import java.util.List;
 
 public interface AnimeService {
-
     Anime saveAnime(Anime anime);
-
     List<Anime> findAll();
-
     Anime updateAnime(Anime anime, Long animeId);
-
     void deleteAnimeById(Long animeId);
-
     List<Anime> findByTitre(String titre);
-
     List<Anime> findByStatut(Statut statut);
 }
