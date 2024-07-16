fcdb0a7b8ff8ee0199d167badf26539d56bcd824
Pierre Lopez <pierre_albert_lopez@hotmail.fr>
2024-07-16 11:58:29 +0200

    Ajout de l'annotation repository a AnimeTitleRepository

--- src/main/java/plopez/biblianime/anime/repository/AnimeTitleRepository.java
+++ src/main/java/plopez/biblianime/anime/repository/AnimeTitleRepository.java
@@ -1,10 +1,12 @@
 package plopez.biblianime.anime.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import plopez.biblianime.anime.entity.AnimeTitle;
 
 import java.util.List;
 
+@Repository
 public interface AnimeTitleRepository extends CrudRepository<AnimeTitle, Long> {
 
     List<AnimeTitle> findByAnimeId(Long animeId);
