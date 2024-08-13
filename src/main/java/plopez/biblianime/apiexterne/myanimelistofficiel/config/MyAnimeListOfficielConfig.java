d9c0b265a5e209ac87916dba9262bf9d64019c34
Pierre Lopez <pierre_albert_lopez@hotmail.fr>
2024-08-13 15:38:03 +0200

    Desactivation bean myanimelistOfficiel non fonctionnel

--- src/main/java/plopez/biblianime/apiexterne/myanimelistofficiel/config/MyAnimeListOfficielConfig.java
+++ src/main/java/plopez/biblianime/apiexterne/myanimelistofficiel/config/MyAnimeListOfficielConfig.java
@@ -1,23 +1,20 @@
 package plopez.biblianime.apiexterne.myanimelistofficiel.config;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 @Deprecated
 @Data
-@Configuration
-@ConfigurationProperties(prefix = "api.myanimelistofficiel")
+//@Configuration
+//@ConfigurationProperties(prefix = "api.myanimelistofficiel")
 public class MyAnimeListOfficielConfig {
 
     private final String titre = "MyAnimeListOfficiel";
 
     private final int requestLimite = 1000000;
 
-    @Value("${base-url}")
+    //@Value("${base-url}")
     private String baseUrl;
 
-    @Value("${client-id}")
+    //@Value("${client-id}")
     private String clientId;
 }
