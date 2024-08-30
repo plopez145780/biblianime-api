83203c3f87d52e5cf93b988562d51be1e2d129bc
Pierre Lopez <pierre_albert_lopez@hotmail.fr>
2024-08-30 15:41:10 +0200

    Suppression message au demarrage sur le constructeur de RequeteExterne
    MESSAGE : HHH000182: No default (no-argument) constructor for class: plopez.biblianime.apiexterne.requeteexternelog.RequeteExterne (class must be instantiated by Interceptor)

--- src/main/java/plopez/biblianime/apiexterne/requeteexternelog/RequeteExterne.java
+++ src/main/java/plopez/biblianime/apiexterne/requeteexternelog/RequeteExterne.java
@@ -3,30 +3,34 @@ package plopez.biblianime.apiexterne.requeteexternelog;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
 public class RequeteExterne {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @NotNull
     @Column(nullable = false, columnDefinition = "timestamp default current_timestamp")
-    private LocalDateTime date;
+    private LocalDateTime date = LocalDateTime.now();
+
     @NotNull
     @Enumerated(EnumType.STRING)
     private ProviderExterne provider;
+
     @NotNull
     private String url;
 
     public RequeteExterne(ProviderExterne provider, String url) {
         this.provider = provider;
-        this.date = LocalDateTime.now();
         this.url = url;
     }
 }
